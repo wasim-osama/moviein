ba20dbaa25308533c9874600d18038a3f193c532
Wasim Akram <contact.wasimosama@gmail.com>
2024-02-12 20:09:23 +0600

    homepage building

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,65 +1,87 @@
 let Movies = [],
-    MovieContainer = document.getElementById('MovieContainer');
+    pnMovieContainer = document.getElementById('pnMovieContainer'),
+    newMovieContainer = document.getElementById('newMovieContainer');
 
+let splide = new Splide( '.splide', {
+    type   : 'loop',
+    padding: '5rem',
+} );
 
-function renderMovies(){
+function showPnMovies(){
     let HTML = '';
-    for (let i = 0; i < Movies.length; i++) {
-        let movie = Movies[i];
+    for (let i = 0; i < Movies.length; i++){
         let genre = '';
-        for (let g = 0; g < movie.genres.length; g++) {
-            genre += '<span class="badge bg-success">'+movie.genres[g]+'</span>';
+        for (let g = 0; g < Movies[i].genres.length; g++){
+            genre += '<span class="badge bg-info me-1">'+Movies[i].genres[g]+'</span>';
         }
-        HTML += `<div class="col-lg-3">
-                <div class="card mb-4">
-                    <img style="width: 100%;height: 310px;object-fit: cover" src="https://www1.yts.nz/`+movie.medium_cover_image+`" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">`+movie.title_english+`</h5>
-                        <p class="card-text">`+movie.rating+`/10</p>
-                        <p class="card-text">
-                            `+genre+`
-                        </p>
-                        <a href="movie.html?movie_id=`+movie.id+`" class="btn btn-primary">View Details</a>
+         HTML += `
+        <div class="col-lg-3 col-md-4">
+            <div class="card text-bg-dark border-light-subtle">
+                <img src="https://www1.yts.nz/`+ Movies[i].medium_cover_image +`"  class="card-img">
+                <div class="card-body text-center">
+                    <h5 class="card-title">`+Movies[i].title_english+`</h5>
+                    <p class="card-text" id="genre">`+genre+`</p>
+                    <p class="card-text"><span class="text-warning"><i class="las la-star"></i></span>`+Movies[i].rating+`/10</p>
+                    <a class="btn btn-danger">Download Now!</a>
+                </div>
+            </div>
+         </div>
+        `;
+
+        pnMovieContainer.innerHTML = HTML;
+    }
+
+}
+function showNewMovies(){
+    let HTML = '';
+    for (let i = 0; i < Movies.length; i++){
+        let genre = '';
+        for (let g = 0; g< Movies[i].genres.length; g++){
+            genre += '<span class="badge bg-info me-1">'+Movies[i].genres[g]+'</span>';
+        }
+        HTML += `
+            <div class="col-lg-3 col-md-4">
+                <div class="card text-bg-dark border-light-subtle">
+                    <img src="https://www1.yts.nz/`+ Movies[i].medium_cover_image +`"  class="card-img">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">`+Movies[i].title_english+`</h5>
+                        <p class="card-text" id="genre">`+genre+`</p>
+                        <p class="card-text"><span class="text-warning"><i class="las la-star"></i></span>`+Movies[i].rating+`/10</p>
+                         <a class="btn btn-danger">Download Now!</a>
                     </div>
                 </div>
-            </div>`;
+            </div>
+        `
+        newMovieContainer.innerHTML = HTML;
     }
-    MovieContainer.innerHTML = HTML;
 }
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
+function getPnMovies(){
+    fetch("http://moviein.test/api/list_movies.php?sort_by=year&minimum_rating=8&limit=4").then(res => {
+        if (!res.ok){
+            throw "error in fetching getPnMovies"
         }
-    }
-})
-function getLatestMovies() {
-    fetch('http://js-script.test/moviedb/api/list_movies.php?sort_by=year&order_by=desc&limit=100').then((res) => {
-        if (!res.ok) {
-            throw new Error('Network response was not ok');
+        return res.json();
+    } ).then(res => {
+        Movies = res.data.movies;
+        showPnMovies();
+    })
+}
+function getNewMovies(){
+    fetch('http://moviein.test/api/list_movies.php?sort_by=year&order_by=desc&limit=4').then(res => {
+        if (!res.ok){
+            throw "error in fetching getNewMovies"
         }
         return res.json();
     }).then(res => {
         Movies = res.data.movies;
-        renderMovies();
-    }).catch(error => {
-        console.error('Error:', error);
+        showNewMovies()
+        console.log(Movies);
     });
 }
 
 
 window.onload = () => {
-    getLatestMovies();
-    $(document).ready(function(){
-        $(".owl-carousel").owlCarousel();
-    });
+    getPnMovies();
+    getNewMovies();
+    splide.mount();
 }
