3a93a1b9cd126b9c7d0583cc424c56ae02533653
Wasim Akram <contact.wasimosama@gmail.com>
2024-02-16 15:15:36 +0600

    single movie page

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,20 @@ let Movies = [],
     pnMovieContainer = document.getElementById('pnMovieContainer'),
     newMovieContainer = document.getElementById('newMovieContainer'),
     actionMovieContainer = document.getElementById('actionMovieContainer'),
-    pMovieContainer = document.getElementById('pMovieContainer');
+    pMovieContainer = document.getElementById('pMovieContainer'),
+    screenSize= window.matchMedia("(max-width: 991px)");
 
 function initSplide (){
     let splide = new Splide( '.splide', {
         type   : 'loop',
         padding: '5rem',
+        pagination: false,
+        autoplay: 'play',
+        breakpoints: {
+            768: {
+                padding: 0,
+            },
+        }
     } );
     splide.mount();
 }
@@ -19,22 +27,56 @@ function charLimit(str,limit){
     return str;
 }
 
+function showActionMovies(){
+    let HTML = '';
+    for(let i = 0; i < Movies.length; i++ ){
+        let genre = '';
+        for (let g = 0; g < Movies[i].genres.length; g++){
+            genre += '<span class="badge bg-accents text-black-50 me-1">'+Movies[i].genres[g]+'</span>'
+        }
+        HTML += `
+        <div class="card slider splide__slide bg-background" data-splide-interval="10000">
+            <img src="https://www1.yts.nz/`+Movies[i].background_image_original+`" class="card-img w-100 h-100 opacity-50">
+            <div class="card-img-overlay overlay-custom">
+                <img class="m-auto h-100" src="https://www1.yts.nz/`+Movies[i].medium_cover_image+`" alt="`+Movies[i].title_english+`">
+            </div>    
+            <div class="start-300p responsive-slider text-light card-img-overlay d-flex flex-column  justify-content-center">
+                <h5 class="card-title fs-1">`+charLimit(Movies[i].title_english,30)+`</h5>
+                <p class="card-text">`+genre+`</p>
+                <p class="card-text">
+                <span class="text-accents fw-bold"><i class="las la-star"></i></span>
+                `+Movies[i].rating+`/10</p>
+                <a href="movie.html?movie_id=`+Movies[i].id+`" class="btn btn-theme">Download Now</a>
+            </div>
+         </div>
+        `
+    }
+    actionMovieContainer.innerHTML = HTML;
+    initSplide ();
+
+}
 function showPnMovies(){
     let HTML = '';
     for (let i = 0; i < Movies.length; i++){
         let genre = '';
         for (let g = 0; g < Movies[i].genres.length; g++){
-            genre += '<span class="badge bg-accents text-black-50 me-1">'+Movies[i].genres[g]+'</span>';
+            if (g < 2){
+                if (!screenSize.matches){
+                    genre += '<span class="badge bg-accents text-black-50">'+Movies[i].genres[g]+'</span>';
+                }else{
+                    genre += '<span class="badge bg-accents text-black-50">'+Movies[i].genres[g].substring(0,3)+'.</span>';
+                }
+            }
         }
          HTML += `
-        <div class="col-lg-3 col-md-4">
+        <div class="col-xxl-3 col-lg-4 col-md-4 col-sm-6 mb-2">
             <div class="card border-0 bg-secondary-cus text-light shadow-sm">
             <div class="img-box">                
                 <img src="https://www1.yts.nz/`+ Movies[i].medium_cover_image +`"  class="card-img w-100 h-100" onerror="this.src='assets/img/poster.jpg';">
             </div>
                 <div class="card-body text-center">
-                    <h5 class="card-title">`+charLimit(Movies[i].title_english, 20)+`</h5>
-                    <p class="card-text" id="genre">`+genre+`</p>
+                    <h6 class="card-title card-title-custom">`+charLimit(Movies[i].title_english, 18)+`</h6>
+                    <p class="card-text">`+genre+`</p>
                     <p class="card-text"><span class="text-warning"><i class="las la-star"></i></span>`+Movies[i].rating+`/10</p>
                     <a class="btn btn-theme">Download Now!</a>
                 </div>
@@ -64,8 +106,8 @@ function showPMovies(){
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title fw-lighter">`+charLimit(Movies[i].title_english, 10)+`</h5>
-                        <p class="card-text fw-lighter">`+genre+`</p>
+                        <h6 class="card-title fw-lighter h6">`+charLimit(Movies[i].title_english, 10)+`</h6>
+                        <p class="card-text fw-lighter genre">`+genre+`</p>
                         <p class="card-text fw-lighter small"><span class="text-accents"><i class="las la-star"></i></span>`+Movies[i].rating+`/10</p>
                     </div>
                 </div>
@@ -78,44 +120,21 @@ function showPMovies(){
     pMovieContainer.innerHTML = HTML;
 
 }
-
-function showActionMovies(){
-    let HTML = '';
-    for(let i = 0; i < Movies.length; i++ ){
-        let genre = '';
-        for (let g = 0; g < Movies[i].genres.length; g++){
-            genre += '<span class="badge bg-accents text-black-50 me-1">'+Movies[i].genres[g]+'</span>'
-        }
-        HTML += `
-        <div class="card slider splide__slide bg-background">
-            <img src="https://www1.yts.nz/`+Movies[i].background_image_original+`" class="card-img w-100 h-100 opacity-50">
-            <div class="card-img-overlay overlay-custom">
-                <img class="m-auto h-100" src="https://www1.yts.nz/`+Movies[i].medium_cover_image+`" alt="`+Movies[i].title_english+`">
-            </div>    
-            <div class="start-300p text-light card-img-overlay d-flex flex-column  justify-content-center">
-                <h5 class="card-title fs-1">`+charLimit(Movies[i].title_english,30)+`</h5>
-                <p class="card-text">`+genre+`</p>
-                <p class="card-text">
-                <span class="text-accents fw-bold"><i class="las la-star"></i></span>
-                `+Movies[i].rating+`/10</p>
-                <a class="btn btn-theme">Download Now</a>
-            </div>
-         </div>
-        `
-    }
-    actionMovieContainer.innerHTML = HTML;
-    initSplide ();
-
-}
 function showNewMovies(){
     let HTML = '';
     for (let i = 0; i < Movies.length; i++){
         let genre = '';
         for (let g = 0; g< Movies[i].genres.length; g++){
-            genre += '<span class="badge bg-accents text-black-50 me-1">'+Movies[i].genres[g]+'</span>';
+            if (g < 2){
+                if (!screenSize.matches){
+                    genre += '<span class="badge bg-accents text-black-50">'+Movies[i].genres[g]+'</span>';
+                }else{
+                    genre += '<span class="badge bg-accents text-black-50">'+Movies[i].genres[g].substring(0,3)+'.</span>';
+                }
+            }
         }
         HTML += `
-            <div class="col-lg-3 col-md-4">
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-2">
                 <div class="card border-0 bg-secondary-cus text-light shadow-sm">
                 <div class="img-box">  
                     <img src="https://www1.yts.nz/`+ Movies[i].medium_cover_image +`"  class="card-img w-100 h-100" onerror="this.src='assets/img/poster.jpg';">
@@ -131,6 +150,18 @@ function showNewMovies(){
         `
     }
     newMovieContainer.innerHTML = HTML;
+}
+function getActionMovies(){
+    fetch('http://moviein.test/api/list_movies.php?sort_by=rating&order_by=desc&genre=action&limit=10').then(res =>{
+        if (!res.ok){
+            throw "error in fetching getActionMovies";
+        }
+        return res.json();
+    }).then(res =>{
+        Movies = res.data.movies;
+        console.log(Movies);
+        showActionMovies();
+    });
 }
 function getPnMovies(){
     fetch("http://moviein.test/api/list_movies.php?sort_by=year&minimum_rating=8&limit=4").then(res => {
@@ -149,13 +180,13 @@ function getPMovies(){
             throw "error in fetching getPnMovies"
         }
         return res.json();
-    } ).then(res => {
+    }).then(res => {
         Movies = res.data.movies;
         showPMovies();
     })
 }
 function getNewMovies(){
-    fetch('http://moviein.test/api/list_movies.php?sort_by=year&order_by=desc&limit=4').then(res => {
+    fetch('http://moviein.test/api/list_movies.php?sort_by=year&order_by=desc&minimum_rating:5&limit=4').then(res => {
         if (!res.ok){
             throw "error in fetching getNewMovies"
         }
@@ -165,24 +196,12 @@ function getNewMovies(){
         showNewMovies();
     });
 }
-function getActionMovies(){
-    fetch('http://moviein.test/api/list_movies.php?sort_by=rating&order_by=desc&genre=action&limit=10').then(res =>{
-        if (!res.ok){
-            throw "error in fetching getActionMovies";
-        }
-        return res.json();
-    }).then(res =>{
-        Movies = res.data.movies;
-        console.log(Movies);
-        showActionMovies();
-    });
-}
+
 
 
 window.onload = () => {
     getPnMovies();
     getNewMovies();
     getActionMovies();
-    getPMovies()
-
+    getPMovies();
 }
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,29 +1,78 @@
-let Movie = null,
-    movie_id = 0,
-    MovieContainer = document.getElementById('MovieContainer');
+let Movie = null;
+const searchParams = new URLSearchParams(window.location.search);
+const movie_id = searchParams.get('movie_id');
+let movieContainer = document.getElementById('movieContainer');
 
+function showSingleMovie(){
+    let castHtml = '';
+    for (let c = 0 ; c < Movie.cast.length; c++){
 
-function renderMovies(){
-    let HTML = ``;
-    MovieContainer.innerHTML = HTML;
+        console.log(Movie.cast[c]);
+        if (c < 4){
+            castHtml +=`
+            <div class="col-sm-3 col-6 py-2">
+                <div class="d-flex flex-column align-items-center text-light">
+                    <div class="h6 text-center fw-light">`+Movie.cast[c].character_name+`</div>
+                    <img src="https://www1.yts.nz/`+Movie.cast[c].url_small_image+`" alt="" class="rounded-circle">
+                    <div class="h5 text-center">`+Movie.cast[c].name+`</div>
+                </div>  
+            </div>
+        `;
+        }
+    }
+    let HTML = `
+          <div class="bg-img w-100 h-100 position-relative" style="background-image: url('https://www1.yts.nz/` + Movie.background_image_original + `')">
+          <div class="bg-overlay position-absolute w-100 h-100 bg-secondary top-0 start-0 opacity-50 z-0"></div>
+            <div class="container-lg py-5 position-relative z-1">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img class="w-100 shadow-sm rounded-3" src="https://www1.yts.nz/` + Movie.large_cover_image + `" alt="">
+                    </div>
+                    <div class="col-lg-6 offset-lg-2 col-md-8">
+                        <div class="h1">` + Movie.title_long + `</div>
+                        <p class="fw-light">` + Movie.description_intro.substring(0, 150) + `&nbsp;<a href="#" class="text-light fw-normal" id="readMore">Read more</a></p>
+                        <div class="fs-4 text-center fw-bold text-accents">Top Casts:
+                            <div class="row">
+                                ` + castHtml + `
+                            </div>
+                        </div>
+                        <div class="fs-4 font-light pb-2 text-accents fw-bold">
+                         Ratings
+                        </div>
+                        <div class="d-flex align-items-center justify-content-between">
+                            <div class="bg-danger d-inline-block fs-3 p-4 rounded-end-pill"> 
+                                <span class="text-accents fw-bold fw-bold fs-3">
+                                    <i class="las la-star"></i>
+                                </span>
+                                `+ Movie.rating +`/10
+                            </div>  
+                            <div class="fs-4 bg-background fs-3 p-4 rounded-start-pill">
+                                Release Year : `+Movie.year+`
+                            </div>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>  
+    `;
+
+    movieContainer.innerHTML= HTML;
 }
-function getSIngleMovies() {
-    fetch('http://js-script.test/moviedb/api/movie_details.php?movie_id='+movie_id).then((res) => {
-        if (!res.ok) {
-            throw new Error('Network response was not ok');
+function  getSingleMovie(){
+    fetch('http://moviein.test/api/movie_details.php?movie_id='+movie_id+'&with_images=true&with_cast=true').then(res => {
+        if (!res.ok){
+            throw Error;
         }
         return res.json();
-    }).then(res => {
+    } ).then(res => {
         Movie = res.data.movie;
         console.log(Movie);
-    }).catch(error => {
-        console.error('Error:', error);
-    });
+        showSingleMovie();
+    })
 }
 
+window.onload = function (){
 
-window.onload = () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    movie_id = urlParams.get('movie_id');
-    getSIngleMovies();
+    getSingleMovie()
+
 }
